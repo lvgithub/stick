0bb1e6b157a517093b43242b1f75cc040f834d86
fly <liuwe>
2017-03-10 20:44:20 +0800

    completed

--- node_modules/.0.0.14@node-tsc/demo/person.ts
+++
@@ -1,4 +0,0 @@
-interface Person {
-    firstname: string;
-    lastname: string;
-}
--- stickTest.js
+++ stickTest.js
@@ -1,32 +1,38 @@
-var stick = require('./stick');
+var Stick = require('./stickPackage');
 
-let buffer = new stick();
-let bytes1 = Buffer.alloc(5).fill(33);
-let bytes2 = Buffer.alloc(512).fill(33);
+let stick = new Stick();
 
 let bytes3 = Buffer.from([0x00, 0x02, 0x66, 0x66, 0x00, 0x04, 0x88, 0x02, 0x11, 0x11]);
 
 let bytes4 = Buffer.from([0x00, 0x02, 0x66, 0x66, 0x00, 0x04, 0x88, 0x02, 0x11]);
 let bytes5 = Buffer.from([0x11]);
-
-let bytes6 = Buffer.from([0x01,0xfe]);
+// 512个字节包
+let bytes6 = Buffer.from([0x01, 0xfe]);
 let bytes7 = Buffer.alloc(510).fill(33);
+// 513个字节包
+let bytes8 = Buffer.from([0x01, 0xff]);
+let bytes9 = Buffer.alloc(511).fill(33);
 
-buffer.onData(function (data) {
+stick.onData(function (data) {
     console.log('receive data,length:' + data.length);
     console.log('receive data,contents:' + JSON.stringify(data));
+    // console.log('receive data,contents:');
 });
 
 // 传入10个字节,一个长度为2,一个为4的数据包
-console.log('传入10个字节,一个长度为2,一个为4的数据包');
-buffer.putData(bytes3);
+console.log('log:传入两个包,一次Put[验证一次性Put数据包]');
+stick.putData(bytes3);
 
 // 传入10个字节,一个长度为2,一个为4的数据包,分两次Put
-console.log('传入10个字节,一个长度为2,一个为4的数据包,分两次Put');
-buffer.putData(bytes4);
-buffer.putData(bytes5);
+console.log('log:传入两个包,分两次Put[验证分两次Put数据包]');
+stick.putData(bytes4);
+stick.putData(bytes5);
 
-console.log('传入512个字节的数据包');
-buffer.putData(bytes6);
-buffer.putData(bytes7);
+console.log('log:传入512个字节的数据包[验证缓冲全满情况]');
+stick.putData(bytes6);
+stick.putData(bytes7);
+
+console.log('log:传入513个字节的数据包[验证缓冲扩增情况]');
+stick.putData(bytes8);
+stick.putData(bytes9);
 
