de5c5d9797f226f3bc8f532e3ef9b1b9acc75df0
FlyAway <jolvmail@gmail.com>
2018-01-28 08:46:56 +0800

    Format test code

--- test/test.MsgCenter.js
+++ test/test.MsgCenter.js
@@ -9,4 +9,5 @@ msgCenter.onMsgRecv(msg => {
 
 msgCenter.putMsg('234')
 msgCenter.putData(Buffer.from([0x00, 0x02, 0x31, 0x32, 0x00, 0x04, 0x31, 0x32, 0x33, 0x34]))
+
 // console.log(msgCenter.publish('123'))
